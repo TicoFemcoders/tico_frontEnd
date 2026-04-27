a5771fd84415152a15bdde60c32db9ff012caee3
Nuclea88 <ingrid.lopov@gmail.com>
2026-04-27 16:12:44 +0200

    refactor: edpoints with params

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,7 @@
 import { api } from "./api";
 
 export const getAllUsers = (page = 0, size = 50) =>
-  api.get(`/api/users?page=${page}&size=${size}`).then(res => res.data.content);
-
+  api.get(`/api/users`, { params: { page, size } }).then(res => res.data.content);
 
 export const getAllAdmins = () =>
   api.get("/api/users/admins").then(res => res.data.content);
@@ -11,12 +10,11 @@ export const getUserById = (userId) =>
   api.get(`/api/users/${userId}`).then(res => res.data);
 
 export const getUserActiveTickets = (userId) =>
-  api.get(`/api/tickets/my-tickets?userId=${userId}`)
+  api.get(`/api/tickets/my-tickets`, { params: { userId } })
     .then(res => res.data.filter(t => t.status !== "CLOSED"));
 
 export const createUser = (formData) =>
   api.post(`/api/users`, formData);
-
 
 export const updateUser = (userId, formData) =>
   api.put(`/api/users/${userId}`, formData);
