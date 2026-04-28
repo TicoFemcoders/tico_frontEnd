2e14746abf7e24b0e0b2083e55878a714837cea9
annahico <annahico@gmail.com>
2026-04-28 10:25:38 +0200

    feat: improved priotity and label

--- src/components/detailTicket/PriorityAndLabelsPanel.jsx
+++ src/components/detailTicket/PriorityAndLabelsPanel.jsx
@@ -7,13 +7,11 @@ import LabelChip from "../common/LabelChip";
 import ConfirmModal from "../modals/ConfirmModal";
 import { useTicketAttributes } from "../../hooks/useTicketAttributes";
 import { TICKET_PRIORITY, PRIORITY_CONFIG } from "../../utils/enums";
-import { useSnackbar } from "notistack";
 
 
 const PriorityAndLabelsPanel = ({ ticket, isAssignedToMe, isClosed, onRefresh }) => {
     const [isEditing, setIsEditing] = useState(false);
     const [openConfirm, setOpenConfirm] = useState(false);
-    const { enqueueSnackbar } = useSnackbar();
 
     const {
         formData, setFormData,
